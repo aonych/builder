494f4aeb05e246466ea0d07147213e88f3d2c785
aonych <aleksandrao995@gmail.com>
2019-04-03 18:17:44 +0200

    add builders

---
+++ src/classic/Bike.java
@@ -0,0 +1,48 @@
+package classic;
+
+import classic.enums.BikeType;
+import classic.enums.FrameType;
+
+public class Bike {
+    private BikeType type;
+    private float wheelsSize;
+    private FrameType frameType;
+    private int wheelsNumber;
+    private boolean electricEngine;
+
+    public BikeType getType() {
+        return type;
+    }
+
+    public void setType(BikeType type) {
+        this.type = type;
+    }
+
+    public void setWheelsSize(float wheelsSize) {
+        this.wheelsSize = wheelsSize;
+    }
+
+    public void setFrameType(FrameType frameType) {
+        this.frameType = frameType;
+    }
+
+    public void setWheelsNumber(int wheelsNumber) {
+        this.wheelsNumber = wheelsNumber;
+    }
+
+    public void setElectricEngine(boolean electricEngine) {
+        this.electricEngine = electricEngine;
+    }
+
+    @Override
+    public String toString() {
+        return "classic.Bike{" +
+                "type=" + type +
+                ", wheelsSize=" + wheelsSize +
+                ", frameType=" + frameType +
+                ", wheelsNumber=" + wheelsNumber +
+                ", electricEngine=" + electricEngine +
+                '}';
+    }
+
+}
